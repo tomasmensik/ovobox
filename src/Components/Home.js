1dec8569d5e2b34d7fa9a9a0bb8ead34c996606b
tomasmensik <tomasmensik2003@seznam.cz>
2023-11-13 22:16:29 +0100

    Update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,7 @@ import BannerImage from "../Assets/home-banner-image.png";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 import { Link } from "react-scroll";
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
   return (
@@ -15,8 +16,22 @@ const Home = () => {
         </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
-            Namíchej si vlastní sušené ovoce!
-          </h1>
+          <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Namíchej si vlastní sušené ovoce!',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Ovoce přímo na míru!',
+        1000,
+        'Daruj někomu náš dárkový balíček!',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '0.9em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+    </h1>
           <p className="primary-text">
           Začni s námi svou vlastní ovocnou revoluci a objev, jaké lahodné dobroty vlastně existují.
           </p>
